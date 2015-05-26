9d9467772cd03da932aad074f2dad0ac1176403d
Alexander Kisten <kist@gurtam.com>
2015-05-26 16:43:05 +0300

    fix

--- build/2.4/ZabbixApiAbstract.php
+++ build/2.4/ZabbixApiAbstract.php
@@ -248,7 +248,7 @@ abstract class ZabbixApiAbstract
         // get file handler
         $fileHandler = @fopen($this->getApiUrl(), 'rb', false, $streamContext);
         if(!$fileHandler)
-            throw new Exception('Could not connect to "'.$this->getApiUrl().'"', 1);
+            throw new Exception('Could not connect to "'.$this->getApiUrl().'"');
 
         // get response
         $this->response = @stream_get_contents($fileHandler);
@@ -259,16 +259,16 @@ abstract class ZabbixApiAbstract
 
         // response verification
         if($this->response === FALSE)
-            throw new Exception('Could not read data from "'.$this->getApiUrl().'"', 2);
+            throw new Exception('Could not read data from "'.$this->getApiUrl().'"');
 
         // decode response
         $this->responseDecoded = json_decode($this->response);
 
         // validate response
         if(!is_object($this->responseDecoded) && !is_array($this->responseDecoded))
-            throw new Exception('Could not decode JSON response.', 3);
+            throw new Exception('Could not decode JSON response.');
         if(array_key_exists('error', $this->responseDecoded))
-            throw new Exception('API error '.$this->responseDecoded->error->code.': '.$this->responseDecoded->error->data, 4);
+            throw new Exception('API error '.$this->responseDecoded->error->code.': '.$this->responseDecoded->error->data);
 
         // return response
         if($resultArrayKey && is_array($this->responseDecoded->result))
